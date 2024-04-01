e5867043500fd26d17d4918e0e88608fef84a569
Shyam Pawar <shyam.pawar3809@gmail.com>
2024-04-01 17:59:12 +0530

    This is new commit made by shyam

--- editor/src/App.js
+++ editor/src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Editor from './Containers/Editor';
-import Home from './Containers/Home';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Editor from "./Containers/Editor";
+import Home from "./Containers/Home";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
+      <div className="App">
         <Switch>
-          <Route path='/' exact component={Home} />
-          <Route path='/:id' exact component = {Editor} />
+          <Route path="/" exact component={Home} />
+          <Route path="/:id" exact component={Editor} />
         </Switch>
-    </div>
+      </div>
+      {/* some comment */}
     </BrowserRouter>
   );
 }
